de163011af699ed9e1418cfaa7acda023ed27f99
noviirna <novi.irnawati@gmail.com>
2021-07-28 12:25:57 +0700

    add learning materials and example for python data type

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ python3.8 <file-name-to-run-with-.py-extensions>
 
 ### Data Type
 In general, python have 2 data types *static* and *dynamic*
-1. Static Data Type
+1. Static Data Type [(example)](https://github.com/noviirna/learn-python/blob/master/datatype-static.py)
    |Type|Description|Example|
    |----|----|----|
    |`String`|represent text|'hello', '123', 'Hello123!*#', "hello", "123", 'Hello123!*#' |
@@ -28,7 +28,7 @@ In general, python have 2 data types *static* and *dynamic*
    |`Complex`|represent imaginary value|1j, 2-6j|
      
    #### Difference between String and Integer  
-   You need remember that if you put numeric value in `'`, it will be stored as String variable. It will be handled differently  in mathematical operation. Take a look at the example below
+   You need remember that if you put numeric value in `'` or `"`, it will be stored as String variable. It will be handled differently  in operation. Take a look at the example below
    ```
    # addition of numeric value with data type integer
    print(5 + 5) # output 10
@@ -36,3 +36,10 @@ In general, python have 2 data types *static* and *dynamic*
    # addition of numeric value with data type string
    print('5' + '5') # output 55
    ```  
+2. Dynamic Data Type [(example)](https://github.com/noviirna/learn-python/blob/master/datatype-dynamic.py)
+   |Type|Description|Example|
+   |----|----|----|
+   |`List`|represent collection of values that is ordered and indexed, the value can have different data type and can have duplicate value|[1, "1", 'Hello', False]|
+   |`Tuple`|same as `List`, but immutable|(1, "1", 'Hello', False)|
+   |`Set`|represent collection of values that is unordered and unindexed, immutable, the value can have different data type and does not allow duplicate values|{"hello","world"}|
+   |`Dictionary`|represent collection of values that is ordered by key (key-value based), changeable, the value can have different data type and does not allow duplicates (for key name)|{"foo":"bar"}|
--- datatype-static.py
+++ datatype-static.py
@@ -1,41 +1,36 @@
 # This is an example on how to assign variable with example value of related datatype
 
-## Integer, represent integers
+## 1. Integer, represent integers
 integerVar = 2021
 integerVar2 = -2021
+print("\n1. Integer")
+print(type(integerVar)) # output: <class 'int'>
+print(type(integerVar2)) # output: <class 'int'>
 
-## Float, represent decimals
+## 2. Float, represent decimals
 floatVar = 1.21
 floatVar2 = -1.21
+print("\n2.Float")
+print(type(floatVar)) # output: <class 'float'>
+print(type(floatVar2)) # output: <class 'float'>
 
-## String, represent text
+## 3. String, represent text
 stringVar = '2021'
 stringVar2 = 'Python123'
+print("\n3.String")
+print(type(stringVar)) # output: <class 'str'>
+print(type(stringVar)) # output: <class 'str'>
 
-## Boolean, represent true / false
+## 4. Boolean, represent true / false
 booleanVar = True
 booleanVar2 = False
+print("\n4.Boolean")
+print(type(booleanVar)) # output: <class 'bool'>
+print(type(booleanVar2)) # output: <class 'bool'>
 
-## Complex, represent imaginary value
+## 5. Complex, represent imaginary value
 complexVar = 1 + 1j
 complexVar2 = 1j
-
-
-# === not a learning materials, variables only for description in terminal ===
-
-prefix='variable '
-newLine = '\n'
-aggregator1 = " with value "
-aggregator2 = " have datatype "
-
-print('Static Data Type Example', newLine, "= " * 30)
-print(prefix, f'{floatVar=}'.split('=')[0],aggregator1, f'{floatVar=}'.split('=')[1], aggregator2, type(floatVar))
-print(prefix, f'{floatVar2=}'.split('=')[0],aggregator1, f'{floatVar2=}'.split('=')[1], aggregator2, type(floatVar2), newLine)
-print(prefix, f'{integerVar=}'.split('=')[0],aggregator1, f'{integerVar=}'.split('=')[1], aggregator2, type(integerVar))
-print(prefix, f'{integerVar2=}'.split('=')[0],aggregator1, f'{integerVar2=}'.split('=')[1], aggregator2, type(integerVar2), newLine)
-print(prefix, f'{stringVar=}'.split('=')[0],aggregator1, f'{stringVar=}'.split('=')[1], aggregator2, type(stringVar))
-print(prefix, f'{stringVar2=}'.split('=')[0],aggregator1, f'{stringVar2=}'.split('=')[1], aggregator2, type(stringVar2), newLine)
-print(prefix, f'{booleanVar=}'.split('=')[0],aggregator1, f'{booleanVar=}'.split('=')[1], aggregator2, type(booleanVar))
-print(prefix, f'{booleanVar2=}'.split('=')[0],aggregator1, f'{booleanVar2=}'.split('=')[1], aggregator2, type(booleanVar2), newLine)
-print(prefix, f'{complexVar=}'.split('=')[0],aggregator1, f'{complexVar=}'.split('=')[1], aggregator2, type(complexVar))
-print(prefix, f'{complexVar2=}'.split('=')[0],aggregator1, f'{complexVar2=}'.split('=')[1], aggregator2, type(complexVar2), newLine)
+print("\n5.Complex")
+print(type(complexVar)) # output: <class 'complex'>
+print(type(complexVar2)) # output: <class 'complex'>
